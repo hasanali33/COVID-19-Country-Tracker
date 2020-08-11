c23e1ff4f9ab5bcfb1c0b0837dd8b92924294dce
hasanali33 <ali.hasan3232@gmail.com>
2020-08-11 01:27:56 -0500

    api in html code, need to switch

---
+++ src/components/countrySelect.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+
+class countrySelect extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+
+
+
+  componentWillRecieveProps() {
+    
+  }
+
+}
+
+export default countrySelect;
---
+++ src/script.js
@@ -0,0 +1,2 @@
+fetch('https://finnhub.io/api/v1/covid19/us')
+  .then(res => res.JSON())
